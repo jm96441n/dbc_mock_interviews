5d755bab7769bdaa54cbcfd93939ddfc6df28971
John Maguire <jmaguire5588@gmail.com>
2016-07-15 13:00:16 -0400

    updated upcoming to make dates unique

--- db/migrate/20160715125056_upcomings.rb
+++ db/migrate/20160715125056_upcomings.rb
@@ -1,7 +1,7 @@
 class Upcomings < ActiveRecord::Migration
   def change
   	create_table :upcomings do |t|
-  		t.date :date, null: false
+  		t.date :date, null: false, unique: true
   		t.references :user, null: false
   	end
   end

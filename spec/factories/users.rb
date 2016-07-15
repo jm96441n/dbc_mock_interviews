d341ff26286fc0cfa1e9e0477d684330c31595c2
John Maguire <jmaguire5588@gmail.com>
2016-07-15 09:24:30 -0400

    added rspec and guard

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence :email do |n|
+    	"person#{n}@example.com"
+    end
   end
+end
+
+FactoryGirl.define do
+	factory :user, class: "User" do
+		email
+		name 'Person McPeopleFace'
+		password '123456'
+		password_confirmation '123456'
+	end
 end

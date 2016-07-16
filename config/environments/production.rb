7f4986770e68d1b8a77b4bae13d5112cfeb2270f
John Maguire <jmaguire5588@gmail.com>
2016-07-15 23:02:15 -0400

    fixing mailer configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.default_method = :smtp
 
   config.action_mailer.smtp_settings {
-    address: 'smtp.sendgrid.net',
+    :address => 'smtp.sendgrid.net',
     port: '587',
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],

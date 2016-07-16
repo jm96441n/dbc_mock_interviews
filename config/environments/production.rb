a4af98c26009a93fbe55505b9e4f71a930f9c69b
John Maguire <jmaguire5588@gmail.com>
2016-07-15 23:04:40 -0400

    trying to fix mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'dbc-algorithms.herokuapp.com' }
-  config.action_mailer.default_method = :smtp
 
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
